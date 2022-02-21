4c4bdc1e88d4ce14e16f6ce6020169baf33dcf7d
juli6464 <juli6464@hotmail.com>
2022-02-21 11:33:36 -0500

    upload

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\PostController;
+use Illuminate\Support\Facades\Route;
+
+
+
+Route::get('/', [PostController::class, 'index'])->name('posts.index');
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
